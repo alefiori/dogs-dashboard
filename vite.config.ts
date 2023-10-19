bb6e505db340f6b87a1fc7e2bf48c911fcd2ee6c
Alessandro Fiori <alefiori97@gmail.com>
2023-10-19 17:43:07 +0200

    chore: add base packages - testing - code format - styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { configDefaults } from 'vitest/dist/config.js'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    exclude: [...configDefaults.exclude, 'src/e2e/*'],
+  },
+  define: {
+    global: 'window',
+  },
 })
